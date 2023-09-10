b7ccb694a1d7c057c1fbaaaa03d769507a242024
JEdoFernandez <juanedozgz@hotmail.com>
2023-09-10 11:43:10 +0200

    Añadimos comando if para que muestre solo valores entre 1-3000

--- src/Main.java
+++ src/Main.java
@@ -8,3 +8,11 @@ public class Main {
         System.out.println("Ingresa un número entre 1-3000");
         numero = leer.nextInt();
 
+        if (numero < 1 || numero > 3000) {
+            System.out.println("El número debe estar entre 1 y 3000");
+        }else{
+            numeroRomano = convertirARomano(numero);
+        System.out.println(numero + " es en números romanos:"+ numeroRomano);
+        }
+
+    }
